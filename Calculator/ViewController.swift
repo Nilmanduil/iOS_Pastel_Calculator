e43f51f9127552f6328c3190b0b8f125216256f9
Thibault Goudouneix <nilmanduil@gmail.com>
2017-10-23 18:00:12 +0200

    Set layout

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -10,9 +10,109 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var resultLabel: UILabel!
+    @IBOutlet weak var operationLabel: UILabel!
+    @IBOutlet weak var storeLabel: UILabel!
+    
+    var inputNumber: Int = 0
+    var storedNumber: Int = 0
+    var operation: String?
+    
+    @IBAction func didPressZero(_ sender: Any) {
+        didPressDigit(digit: 0)
+    }
+    @IBAction func didPressOne(_ sender: Any) {
+        didPressDigit(digit: 1)
+    }
+    @IBAction func didPressTwo(_ sender: Any) {
+        didPressDigit(digit: 2)
+    }
+    @IBAction func didPressThree(_ sender: Any) {
+        didPressDigit(digit: 3)
+    }
+    @IBAction func didPressFour(_ sender: Any) {
+        didPressDigit(digit: 4)
+    }
+    @IBAction func didPressFive(_ sender: Any) {
+        didPressDigit(digit: 5)
+    }
+    @IBAction func didPressSix(_ sender: Any) {
+        didPressDigit(digit: 6)
+    }
+    @IBAction func didPressSeven(_ sender: Any) {
+        didPressDigit(digit: 7)
+    }
+    @IBAction func didPressEight(_ sender: Any) {
+        didPressDigit(digit: 8)
+    }
+    @IBAction func didPressNine(_ sender: Any) {
+        didPressDigit(digit: 9)
+    }
+    func didPressDigit(digit: Int) {
+        if inputNumber == 0 {
+            inputNumber = digit
+        } else {
+            let strInput: String = String(describing: inputNumber) + String(describing: digit)
+            
+            print(inputNumber)
+            print(digit)
+            
+            inputNumber = Int(strInput)!
+        }
+        refreshNumber()
+    }
+    
+    func refreshNumber() {
+        resultLabel.text = (inputNumber == 0 ? "" : String(describing: inputNumber))
+        storeLabel.text = String(describing: storedNumber)
+    }
+    
+    
+    @IBAction func didPressOperator(_ sender: Any) {
+        guard let operatorButton = sender as? UIButton else {
+            return
+        }
+        
+        operationLabel.text = operatorButton.titleLabel?.text
+        
+        if storedNumber == 0 {
+            toStorage()
+        }
+    }
+    
+    func toStorage() {
+        storedNumber = inputNumber
+        inputNumber = 0
+        refreshNumber()
+    }
+    
+    @IBAction func didPressEqual(_ sender: Any) {
+        if storedNumber == 0 {
+            toStorage()
+        }
+    }
+    
+    @IBAction func didPressCancel(_ sender: Any) {
+        inputNumber = 0
+        resultLabel.text = ""
+        storedNumber = 0
+        storeLabel.text = ""
+        operationLabel.text = ""
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // self.view.backgroundColor = .red
+            }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        view.backgroundColor = .yellow
     }
 
     override func didReceiveMemoryWarning() {
